47c960404f783a35acb23e7b924a9f5236f84d4c
Marcos Marado <mmarado@cyngn.com>
2014-12-16 19:31:32 +0000

    CM12vification
    Change-Id: I648f47fa681c1720aac62e83844d2c901c31b58f
    
    Build mm-video-v4l2 on msm8909's
    
    Issue-Id: FEIJ-6
    
    Change-Id: I3eed6c173e1fe2773ad1edebeb4a8035846a470d

--- Android.mk
+++ Android.mk
@@ -1,5 +1,4 @@
 QCOM_MEDIA_ROOT := $(call my-dir)
-$(warning target list is : $(MSM_VIDC_TARGET_LIST))
 
 #Compile these for all targets under QCOM_BOARD_PLATFORMS list.
 ifeq ($(call is-board-platform-in-list, $(QCOM_BOARD_PLATFORMS)),true)
@@ -8,9 +7,10 @@ include $(QCOM_MEDIA_ROOT)/libstagefrighthw/Android.mk
 endif
 
 ifneq ($(TARGET_SUPPORTS_WEARABLES),true)
-ifeq ($(call is-board-platform-in-list, $(MSM_VIDC_TARGET_LIST)),true)
+ifneq ($(filter msm8909 msm8916 msm8939,$(TARGET_BOARD_PLATFORM)),)
 include $(QCOM_MEDIA_ROOT)/mm-video-v4l2/Android.mk
 include $(QCOM_MEDIA_ROOT)/libc2dcolorconvert/Android.mk
 
 endif
 endif
+
